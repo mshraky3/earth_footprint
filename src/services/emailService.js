f85c07efd8a72b96feeaf836ec4a1d4adb1b77af
mahmod al-sharaky <93828510+mshraky3@users.noreply.github.com>
2025-10-17 22:45:12 +0300

    Add files via upload

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://earth-footprint-backend.vercel.app' 
+  ? 'https://apiearthfootprint.vercel.app' 
   : 'http://localhost:5000';
 
 const api = axios.create({
